491d91d8ad651336f6f478d730ae81c5c1893482
Fernando Mujica <imujica@agetic.gob.bo>
2021-02-14 11:38:25 -0400

    polybar para monitores sin nombre

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,13 @@ killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main -c  $HOME/.config/polybar/config.ini &
+  done
+else
+  polybar --reload main -c  $HOME/.config/polybar/config.ini &
+fi
 # Launch polybar
-polybar main -c $HOME/.config/polybar/config.ini &
-polybar main2 -c $HOME/.config/polybar/config.ini &
+#polybar main -c $HOME/.config/polybar/config.ini &
+#polybar main2 -c $HOME/.config/polybar/config.ini &
